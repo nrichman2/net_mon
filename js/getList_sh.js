87c1ea59197634b522ecf0641742b6c8b7887da9
nate <nrichman@wisc.edu>
2017-12-13 14:54:43 -0600

    Added logging to /var/log/net_mon/net_mon.log.  Also added name and interface to table

--- js/getList_sh.js
+++ js/getList_sh.js
@@ -1,20 +1,31 @@
 var cp = require('child_process');
 var fs = require('fs');
 var mysql = require('mysql');
+var moment = require('moment');
+
+function log(message){
+	var toWrite = '[getList] '+moment().format('MMM Do YYYY, hh:mm:ss')+': '+message+'\n';
+	fs.appendFileSync('/var/log/net_mon/net_mon.log',toWrite);
+}
+
 var list;
 function getList(){
 	var quit=0;
 	var  mine = cp.exec('sh /home/nrichman/Documents/net_mon/shell/getList.sh',
   	(error, stdout, stderr) => {
     	list = JSON.parse(stdout);
+			log("Received list from netbeez");
     	//console.log(`${stdout}`);
 			//console.log(`${stderr}`);
     });
 
 }
 function parseList(){
+
+
 	if(list ==null){
-		console.log('list not there');
+		//console.log('list not there');
+		log('List not there');
 		return;
 	}
 
@@ -29,43 +40,68 @@ function parseList(){
 	var agents = list.agents;
 	//console.log(agents[0]);
 	con.connect(function(err){
-			if(err) throw err;
-			//console.log("connected");
-			return;
-		});
-		//console.log(agents);
-		for(var i=1; i<Object.keys(agents).length; i++){
-			var agent = agents[i];
-			var mac;
-			var ip;
-			var gateway;
-			var query = "INSERT INTO agents (mac, ip, gateway) VALUES (";
-			if(agent.network_interfaces.wlan0){
-				mac = '\''+agent.network_interfaces.wlan0.mac+'\'';
-				ip = '\''+agent.network_interfaces.wlan0.ip_address+'\'';
-				gateway = '\''+agent.network_interfaces.wlan0.gateway+'\'';
-				query += mac+","+ip+","+gateway+") ON DUPLICATE KEY UPDATE ip="+ip+", gateway="+gateway+";"
-				console.log(query);
-				con.query(query, function(err, result){
-						if(err) throw err;
-						//console.log(result);
-					});
-			} else {
-				mac = agent.network_interfaces.eth0.mac;
-				ip = agent.network_interfaces.eth0.ip_address;
-				gateway = agent.network_interfaces.eth0.gateway;
-				query += mac+","+ip+","+gateway+");";
-				console.log(query);
-				con.query(query, function(err, result){
-						if(err) console.log(err);
-						//console.log(result);
-					});
-			}
+		if(err){
+			log(err.message);
+			throw err;
 		}
-	con.query("SHOW FIELDS IN agents", function(err, result){
-			if(err) throw err;
-			console.log(result);
-		});
+		log("Connected to database");
+		return;
+	});
+	//console.log(agents);
+	for(var i=0; i<Object.keys(agents).length; i++){
+		var agent = agents[i];
+		var mac;
+		var ip;
+		var gateway;
+		var query_temp = "INSERT INTO agents (mac, ip, gateway, interface,name) VALUES (";
+		if(agent.network_interfaces.eth0){
+			var query = query_temp
+			name = '\''+agent.name+'\'';
+			if(agent.network_interfaces.wlan0){
+			mac = '\''+agent.network_interfaces.wlan0.mac+'\'';
+			ip = '\''+agent.network_interfaces.wlan0.ip_address+'\'';
+			gateway = '\''+agent.network_interfaces.wlan0.gateway+'\'';
+			key = '\''+agent.network_interfaces.wlan0.key+'\'';
+			query += mac+","+ip+","+gateway+","+key+", "+name+") ON DUPLICATE KEY UPDATE ip="+ip+", gateway="+gateway+", interface="+key+",name="+name+";";
+			con.query(query, function(err, result){
+				if(err){
+					log(err.message);
+					throw err;
+				}
+				log("Wrote to db");
+			});
+			}
+			mac_e = '\''+agent.network_interfaces.eth0.mac+'\'';
+			ip_e = '\''+agent.network_interfaces.eth0.ip_address+'\'';
+			if(!ip_e) console.log("null")
+			gateway_e = '\''+agent.network_interfaces.eth0.gateway+'\'';
+			key_e = '\''+agent.network_interfaces.eth0.key+'\'';
+			var query_e = query_temp + mac_e+","+ip_e+","+gateway_e+","+key_e+", "+name+") ON DUPLICATE KEY UPDATE ip="+ip_e+", gateway="+gateway_e+", interface="+key_e+", name="+name+";";
+			console.log(query);
+			console.log(query_e);
+
+			con.query(query_e, function(err, result){
+				if(err){
+					log(err.message);
+					throw err;
+				}
+				log("Wrote to db");
+			});
+		} else {
+			mac = agent.network_interfaces.eth0.mac;
+			ip = agent.network_interfaces.eth0.ip_address;
+			gateway = agent.network_interfaces.eth0.gateway;
+			query += mac+","+ip+","+gateway+");";
+			console.log(query);
+			con.query(query, function(err, result){
+				if(err){
+					log(err.message);
+					throw err;
+				}
+				log("Wrote to db");
+			});
+		}
+	}
 	con.end();
 }
 getList();
