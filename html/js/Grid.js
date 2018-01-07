90e419fa64af5d902956d362c3bdd89169cdfe0a
nate <nrichman@wisc.edu>
2018-01-07 16:50:18 -0600

    Added some api functions, more to add later as it seems they arent implemented by netbeez.  Worked on the grid, having issues with enter, will address later

--- html/js/Grid.js
+++ html/js/Grid.js
@@ -5,16 +5,70 @@ var height = $(window).height();
 var agents = []; //[[animal_user,animal_mgmt],
             //[cssc_user,cssc_mgmt],[nm_user,nm_mgmt]]
 
+var SIZE = 20;
+var PADDING = 20;
+function generateIndivData(){
+
+  var matrix = [];
+  for(var row = 0; row<3; row++){
+
+    var column = [];
+    for(var col = 0; col<3; col++){
+      var baseGateway = 192168001001;
+      var baseTarget = 192168255255;
+      var obj = {
+        "agentObj": {"gateway" : baseGateway},
+        "targetObj": {"ip" : baseTarget},
+        "xpos": 0,
+        "ypos": 0,
+        "width": SIZE,
+        "height": SIZE,
+        "row" : 0
+      };
+      baseGateway++;
+      baseGateway--;
+      column.push(obj);
+    }
+    matrix.push(column);
+  }
+  return matrix;
+}
+
+//console.log(generateData())
 var CELLSIZE = 25;
 function init(){
 
-  var grid = d3.select("#grid").append("svg")
+  var data = generateIndivData();
+  var svg = d3.select("#main")
+    .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-  var animal = grid.append("g")
-    .attr("transform","translate("+0+","+0+")")
+  var grid = svg.append("g")
+    .attr("transform","translate("+0+","+0+")");
+
+  var rows = grid.selectAll(".row")
+    .data(data)
+      .enter()
+      .append("g")
+      .attr("class", "row");
+
+  var cells = rows.selectAll(".cell")
+    .data(function(d,i){    //d is an array, d is current value, i is index
+      d.map(function(obj){ //sets obj at i to have row of i, returns obj
+        obj.row = i;
+        return obj;
+      })
+    });
+  cells.enter()
+    .append("rect")
+    .attr("width", SIZE)
+    .attr("height", SIZE)
+    .attr("transform","translate(0,0)");
+  cells.exit()
+    .remove();
+  cells.attr("transform", function(d, i){ return "translate("+i*SIZE+","+d[i].row*SIZE+")"});
 
 }
-
+console.log()
 init();
--- html/js/swagger.js
+++ html/js/swagger.js
@@ -1,7 +1,18 @@
+var swagger = "https://wisc.netbeezcloud.net/"
 function getAgents(){
-  var data = {
-    "Authorization" : "",
-    "API-VERSION" : "v1"
-  }
-  $().getJSON('https://wisc.netbeezcloud.net/agents.json')
+//  var data = {
+    //Authorization : "d4fbd8f9bc8dc6b3933297f33a653d86cbae7a56",
+    //API-VERSION : "v1";
+  //}
+  return $.getJSON(swagger+'agents/active.json');//, data);
 }
+var data = {
+  "Authorization" : "d4fbd8f9bc8dc6b3933297f33a653d86cbae7a56",
+  "API-VERSION" : "v1",
+  "test_type_id" : 1
+};
+function getPingStatusByAgent(id){
+  return $.getJSON(swagger+'agents/'+id+'test_statuses_by_type.json',data);
+}
+
+console.log(getPingStatusByAgent(2));
