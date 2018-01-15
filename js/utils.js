a7c649bea68c6d97d948cc5bdbe66ced58b035c1
nate <nrichman@wisc.edu>
2018-01-15 12:50:52 -0600

    fixed untracked files

--- js/utils.js
+++
@@ -1,6 +0,0 @@
-var moment = require('moment');
-var fs = require('fs');
-module.exports.progLog =  function(program,message){
-	   var toWrite = program+moment().format('MMM Do YYYY, hh:mm:ss')+': '+message+'\n';
-	    fs.appendFileSync('/var/log/net_mon/net_mon.log',toWrite);
-    }
