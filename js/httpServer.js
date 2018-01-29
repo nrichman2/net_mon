363bf51956983d1785dca5ff82af39a4269729ce
nate <nrichman@wisc.edu>
2018-01-29 17:54:49 -0600

    Fixed the mysql interaction, now pulls all targets at once and parses that list instead of pulling each individually.  Also added some of the response logic in httpServer.  Need to add functions in mysql get to return array of all targets/agents and their corresponding matrix.

--- js/httpServer.js
+++ js/httpServer.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var mysql = require('mysql');
 var progLog = require('./utils.js').progLog;
+var sql_interact = require('./mySQLget.js');
+var url = require('url');
 
 var config_file = require("./config.json");
+
 
 var config = JSON.parse(JSON.stringify(config_file));
 var con = mysql.createConnection({
@@ -20,25 +23,53 @@ con.connect(function(err){	//connect to database
   progLog("[Server] Connected to database");
   return;
 });
-function getAgents(callback){
-  var query = "SELECT * FROM agents"
-  var agents = "";
-  //todo create tables in mysql for vlans
-  con.query(query, function(err, rows){
-    callback(err, rows);
-  });
-}
-
-
+var getter = new sql_interact.mySQLget(con);
 
 var listener = function(request, response){
-  var url = request.url;
-  var agents = "";
-  response.writeHead('200', {'Content-type': 'application/json'});
-  agents = getAgents(function(err, rows){
-    progLog("[Server] responding with agents JSON");
-    response.end(JSON.stringify(rows));
-  });
+  progLog("[Server] new request from "+request.headers["user-agent"]);
+  var myUrl = url.parse(request.url,true);
+  console.log(myUrl.query);
+  var area = myUrl.query.area;
+  //Parses and sends response
+  switch(myUrl.pathname){
+    case "/agents.json":
+      response.writeHead('200', {'Content-type': 'application/json'});
+
+      getter.getAgents(area,function(data){
+        console.log(rows);
+      });
+      response.end("{\"agents\":\"not done yet\"}");
+      break;
+    case "/targets.json":
+      response.writeHead('200', {'Content-type': 'application/json'});
+      getter.getAllTargetIds(function(data){
+        console.log(data);
+      })
+      //getter.getTargets(area, function(data){
+      //  response.end(data);
+      //});
+
+      break;
+    case "/matrix.json":
+      response.writeHead('200', {'Content-type': 'application/json'});
+
+      getter.getTargetIds(area, function(data){
+        getter.generateMatrix(data, function(matrix){
+          response.end(JSON.stringify(matrix));
+        });
+      });
+      break;
+    default:
+      response.writeHead('404', {'Content-type': 'text/html'});
+      response.end("<H1>Not Found or implemented yet</h1>");
+      break;
+  }
+
+  //getter.
+  //agents = getAgents(function(err, rows){
+  //  progLog("[Server] responding with agents JSON");
+  //  response.end(JSON.stringify(rows));
+  //});
 
 }
 const port = 4000;
--- js/mySQLget.js
+++ js/mySQLget.js
@@ -2,139 +2,84 @@
 var mysql = require('mysql');
 var progLog = require('./utils.js').progLog;
 
-var config_file = require('./config.json');
-config = JSON.parse(JSON.stringify(config_file));
-var con = mysql.createConnection({
-  host: "localhost",
-  user: config.username,
-  password: config.password,
-  database: config.database
-});
-con.connect(function(err){	//connect to database
-  if(err){
-    progLog("[Server] "+err.message);
-    throw err;
-  }
-  console.log("connected");
-  progLog("[Server] Connected to database");
-  return;
-});
+module.exports.mySQLget = function(con){
 
-var mySQLget = function(con){
-
-
-  //
-  this.generateMatrix = function(targetList){
+  //generate matrix of agents and targets for one area, takes area target list and a callback from httpServer
+  this.generateMatrix = function(targetList, callback){
     var matrix = [];
+    var query = "select json_extract(agentids, '$[*]'),id from targets";
+    con.query(query, function(err, rows){
 
-    var column =0;
-    targetList.forEach(function(target){
-      var query = "select json_extract(agentids, '$[*]') from targets where id=";
-      var id = target.id;
-      query+= "\'"+id+"\';";
-      console.log(column+" outer")
-      con.query(query, function(err, rows){
-        //pass list of the form for each target [{"test_id": , "agent_id": },...]
-        //console.log(JSON.parse(rows));
-        makeColumn(err, rows, matrix, column, id);
-      });
-      column++;
-    });
-    //console.log(matrix);
-  }
-
-
-  //Code from
-
-  function async(arg, callback){
-    setTimeout(function() {callback(arg);}, 200);
-
-  }
-
-  function final() {}
-  function helper(con,query){
-      var query = "select json_extract(agentids, '$[*]') from targets where id=";
-    con.query
-  }
-
-  this.makeMat = function(matrix, index, list, con){
-
-    var query = "select json_extract(agentids, '$[*]') from targets where id=";
-    var id = list[index].id;
-    query+= "\'"+id+"\';";
-    console.log(id);
-    if(index < list.length){
-      con.query(query, function(err, rows){
         var weirdQuery = 'json_extract(agentids, \'$[*]\')';
-        var list = [];
-        try{
-          //data returned is in rawdata stream json stringify turns that into json string, then need to parse
-          //because mysql returns query and row, in list form, need to access element [0] and then the value
-          //at the query [weirdQuery]
-          list = JSON.parse(JSON.parse(JSON.stringify(rows))[0][weirdQuery]);
-        } catch (err){
-          progLog("[mySQLget] parsing JSON error");
-        }
-        matrix[index] = new Array(list.length);
-        for(var j = 0; j<list.length; j++){
+        var list = JSON.parse(JSON.stringify(rows));
 
-          list[j].target_id = id;
-          matrix[index][j] = list[j];
-        }
-        var pass = index++;
-        mySQLget.makeMat(matrix,pass,list,con);
-      });
-    }
+        list.forEach(function(datum, i){
+
+          var tests = JSON.parse(datum[weirdQuery]);
+          var id = datum['id'];
+          matrix[i] = new Array(tests.length);
+
+          tests.forEach(function(test,j){
+            test.id = id;
+            matrix[i][j] = test;
+          });
+
+        });
+        callback(matrix);
+    });
   }
-
-  function makeColumn(err, rows, matrix, column, id){
-    console.log(column+" inner");
-    var list = [];
-    var weirdQuery = 'json_extract(agentids, \'$[*]\')';
-    try{
-      //data returned is in rawdata stream json stringify turns that into json string, then need to parse
-      //because mysql returns query and row, in list form, need to access element [0] and then the value
-      //at the query [weirdQuery]
-      list = JSON.parse(JSON.parse(JSON.stringify(rows))[0][weirdQuery]);
-    } catch (err){
-      progLog("[mySQLget] parsing JSON error");
-    }
-    for(var j = 0; j<list.length; j++){
-      matrix[column] = new Array(list.length);
-      list[j].target_id = id;
-      matrix[column][j] = list[j];
-    }
-
-  }
-
 
   //String of areaname
   //callback to handle response
-  this.getTargets = function(area, callback){
+  this.getTargetIds = function(area, callback){
     //maybe dont need to select * possible just ip, or id
     var query = "select id from targets where area=\'"+area+"\';";
     con.query(query, function(err, rows){
       //passes rows into callback
-      callback(err,rows);
+      callback(JSON.stringify(rows));
+    });
+  }
+  this.getTargets = function(area, callback){
+    //maybe dont need to select * possible just ip, or id
+    var query = "select * from targets where area=\'"+area+"\';";
+    con.query(query, function(err, rows){
+      //passes rows into callback
+      callback(JSON.stringify(rows));
     });
   }
 
-
   //String of areaname
   //callback to handle response
-  this.getAgets = function(area, callback){
+  this.getAgents = function(area, callback){
     //maybe dont need to select * possible just ip, or id
     var query = "select * from agents where area=\'"+area+"\';";
     con.query(query, function(err, rows){
       //passes rows into callback
+      console.log(rows);
       callback(err,rows);
     });
   }
-}
 
-var thisMySQL = new mySQLget(con);
-var matrix = [];
-thisMySQL.getTargets("animal", function(err, rows){
-  thisMySQL.makeMat(matrix,0,JSON.parse(JSON.stringify(rows)),con);
-  //thisMySQL.generateMatrix(JSON.parse(JSON.stringify(rows)));
-});
+  this.getAllAgents = function(callback){}
+
+ Need to figure this out.
+  this.getAllTargetIds = function(callback){
+    var query = "select id,area from targets;"
+    con.query(query, function(err, rows){
+      var fullList = [];
+      var list = JSON.parse(JSON.stringify(rows));
+      list.forEach(function(datum){
+        fullList[datum.area] = new Array();
+        fullList[datum.area].push(datum);
+      });
+      callback(fullList);
+    });
+  }
+  this.getAllTargets = function(callback){
+    var query = "select * from targets;"
+    con.query(query, function(err, rows){
+      //console
+    });
+  }
+  this.getAllMatrix = function(targetList,callback){}
+}
